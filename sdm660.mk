88de87fd24f22b2f2dd3ac20401f7d1e48c90822
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    sdm660-common: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- sdm660.mk
+++ sdm660.mk
@@ -373,6 +373,12 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_asus
