b0d03be8b8503e13a2a274f7876af692e69d583c
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-12 13:21:54 +0530

    sdm660-common: Add Pixel Overlays
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Change-Id: I5d8347e44d6954b447e88044c1dfec19d64b088b

--- sdm660.mk
+++ sdm660.mk
@@ -289,6 +289,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+   PixelOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
